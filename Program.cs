21cada458ca0140f0512b3c9cb24137e40264ae1
admin <admin@VTORAYA0>
2022-10-17 21:06:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DllImportProject
+{
+    internal class Program
+    {
+        [DllImport("CppDll.dll", EntryPoint = "PrintHello")]
+        public static extern void MyPrintHello();
+
+        [DllImport("CppDll.dll", EntryPoint = "GaussSum")]
+        public static extern int MyGaussSum(int number);
+        static void Main(string[] args)
+        {
+            //MyPrintHello();
+            Console.WriteLine(MyGaussSum(1000));
+        }
+    }
+}
